4511274998b9dc98fdb9a8726b8bcae70f22ea23
Nick Mertens <nick.mertens@fphcare.co.nz>
2018-06-05 08:23:02 +1200

    Log pageview with GA on page load

--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ import './index.css';
 import App from './App';
 
 ReactGA.initialize('UA-22828082-3');
+ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.render(<App />, document.getElementById('root'));
